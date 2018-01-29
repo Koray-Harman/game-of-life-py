8a59a7c3237947b1c7d527d872fbafe00a782a77
Koray Harman <koray.h@hotmail.com>
2018-01-29 20:49:30 +1100

    update make.bat to execute pylint report

--- make.bat
+++ make.bat
@@ -8,12 +8,20 @@ if "%1" == "help" (
 	:help
 	echo.Please use `make ^<target^>` where ^<target^> is one of
 	echo.  test      	to execute nosetests
+	echo.  pylint      	to execute pylint report
 	echo.  placeholder	to make HTML files named index.html in directories
 	goto end
 )
 
 if "%1" == "test" (
 	nosetests tests
+	if errorlevel 1 exit /b 1
+	echo.
+	goto end
+)
+
+if "%1" == "pylint" (
+	pylint gameoflife
 	if errorlevel 1 exit /b 1
 	echo.
 	goto end
